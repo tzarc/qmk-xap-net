cd911ef79ef8dd9e9b2776889337e8d650733a24
Nick Brassel <nick@tzarc.org>
2026-07-14 12:50:06 +1000

    More info from Xap.Scanner.

--- samples/Xap.Scanner/Program.cs
+++ samples/Xap.Scanner/Program.cs
@@ -38,14 +38,39 @@ static async Task PrintDeviceAsync(DeviceInfo info)
             return;
         client = await XapClient.CreateAsync(device);
 
-        var xapVersion = await client.Xap.GetVersionAsync();
-        var qmkVersion = client.Qmk.HasVersionQuery
-            ? (await client.Qmk.GetVersionAsync()).ToString()
-            : "n/a";
-
         Console.WriteLine(device.ToString());
-        Console.WriteLine($"  XAP version: {xapVersion}");
-        Console.WriteLine($"  QMK version: {qmkVersion}");
+        Console.WriteLine($"  XAP version:        {await client.Xap.GetVersionAsync()}");
+        if (client.Xap.HasGetSecureStatus)
+            Console.WriteLine($"  Secure status:      {await client.Xap.GetSecureStatusAsync()}");
+
+        if (client.Qmk.HasVersionQuery)
+            Console.WriteLine($"  QMK version:        {await client.Qmk.GetVersionAsync()}");
+        if (client.Qmk.HasGetBoardIdentifiers)
+        {
+            var id = await client.Qmk.GetBoardIdentifiersAsync();
+            Console.WriteLine($"  Board identifiers:  {id.VendorId:X4}:{id.ProductId:X4} rev {id.ProductVersion:X4}, QMK id 0x{id.QmkUniqueIdentifier:X8}");
+        }
+        if (client.Qmk.HasGetBoardManufacturer)
+            Console.WriteLine($"  Manufacturer:       {await client.Qmk.GetBoardManufacturerAsync()}");
+        if (client.Qmk.HasGetProductName)
+            Console.WriteLine($"  Product:            {await client.Qmk.GetProductNameAsync()}");
+        if (client.Qmk.HasGetHardwareId)
+            Console.WriteLine($"  Hardware ID:        {string.Concat((await client.Qmk.GetHardwareIdAsync()).Select(u => u.ToString("X8")))}");
+        if (client.Qmk.HasGetConfigBlobLength)
+            Console.WriteLine($"  Config blob:        {await client.Qmk.GetConfigBlobLengthAsync()} bytes");
+
+        if (client.Keymap.HasGetLayerCount)
+            Console.WriteLine($"  Keymap layers:      {await client.Keymap.GetLayerCountAsync()}");
+        if (client.Remapping.HasGetDynamicLayerCount)
+            Console.WriteLine($"  Remappable layers:  {await client.Remapping.GetDynamicLayerCountAsync()}");
+
+        if (client.Lighting.HasBacklight && client.Lighting.Backlight.HasGetEnabledEffects)
+            Console.WriteLine($"  Backlight effects:  0x{await client.Lighting.Backlight.GetEnabledEffectsAsync():X2}");
+        if (client.Lighting.HasRgblight && client.Lighting.Rgblight.HasGetEnabledEffects)
+            Console.WriteLine($"  RGB light effects:  0x{await client.Lighting.Rgblight.GetEnabledEffectsAsync():X16}");
+        if (client.Lighting.HasRgbMatrix && client.Lighting.RgbMatrix.HasGetEnabledEffects)
+            Console.WriteLine($"  RGB matrix effects: 0x{await client.Lighting.RgbMatrix.GetEnabledEffectsAsync():X16}");
+
         Console.WriteLine();
     }
     catch (Exception ex)
